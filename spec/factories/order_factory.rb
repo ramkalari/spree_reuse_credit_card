89f8e3a05faf2b2da3fab15878f19e8bc7dd36b3
Daniel Wright <daniel@godynamo.com>
2013-02-26 16:07:04 -0500

    Use supported FactoryGirl invocation syntax

--- spec/factories/order_factory.rb
+++ spec/factories/order_factory.rb
@@ -1,25 +1,25 @@
 FactoryGirl.define do
   factory :order_in_address_state, parent: :order do
-    after(:build) do |order|
+    after_build do |order|
       order.line_items << FactoryGirl.create(:line_item, order: order)
     end
 
-    after(:create) { |order| order.next! }
+    after_create { |order| order.next! }
   end
 
   factory :order_in_delivery_state, parent: :order_in_address_state do
-    after(:create) { |order| order.next! }
+    after_create { |order| order.next! }
   end
 
   factory :order_in_payment_state, parent: :order_in_delivery_state do
-    after(:create) { |order| order.next! }
+    after_create { |order| order.next! }
   end
 
   factory :order_in_confirm_state, parent: :order_in_payment_state do
-    after(:create) { |order| order.next! }
+    after_create { |order| order.next! }
   end
 
   factory :order_in_complete_state, parent: :order_in_confirm_state do
-    after(:create) { |order| order.next! && order.finalize! }
+    after_create { |order| order.next! && order.finalize! }
   end
 end
