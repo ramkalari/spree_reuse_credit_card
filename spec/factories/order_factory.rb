1b979c11fc2aae65ab5e0e3a5fad53e4f6c1bedb
Daniel Wright <daniel@godynamo.com>
2013-02-26 11:31:32 -0500

    Cleans up formatting of custom Order factories

--- spec/factories/order_factory.rb
+++ spec/factories/order_factory.rb
@@ -1,45 +1,25 @@
 FactoryGirl.define do
-
-  factory :order_in_address_state, :parent => :order do
-    after_build { |order|
-      order.line_items << Factory(:line_item, :order => order)
-    }
-
-    after_create { |order|
-      order.next!
-    }
-  end
-
-  factory :order_in_delivery_state, :parent => :order_in_address_state do
-    after_create { |order| order.next! }
-  end
-
-  factory :order_in_payment_state, :parent => :order_in_delivery_state do
-    after_create { |order| 
-      order.next! 
-    }
-  end
-
-  factory :order_in_confirm_state, :parent => :order_in_payment_state do
-    after_create do |order|
-      order.next!
+  factory :order_in_address_state, parent: :order do
+    after(:build) do |order|
+      order.line_items << FactoryGirl.create(:line_item, order: order)
     end
+
+    after(:create) { |order| order.next! }
   end
 
-  factory :order_in_complete_state, :parent => :order_in_confirm_state do
-    after_create { |order| order.next! && order.finalize! }
+  factory :order_in_delivery_state, parent: :order_in_address_state do
+    after(:create) { |order| order.next! }
   end
 
+  factory :order_in_payment_state, parent: :order_in_delivery_state do
+    after(:create) { |order| order.next! }
+  end
 
+  factory :order_in_confirm_state, parent: :order_in_payment_state do
+    after(:create) { |order| order.next! }
+  end
 
-#  factory :order_in_payment_state, :parent => :order do
-#    after_create { |order|
-#      # order.line_items << Factory(:line_item, :order => order)
-#
-#      order.next!
-#      order.next!
-#      order.next!
-#
-#    }
-#  end
+  factory :order_in_complete_state, parent: :order_in_confirm_state do
+    after(:create) { |order| order.next! && order.finalize! }
+  end
 end
